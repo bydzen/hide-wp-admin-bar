c4a4ec564f4e10b595d5cf44e0e33c2f0cb37cd5
bydzen <svnteentthones@gmail.com>
2024-07-24 14:27:04 +0700

    Add main file to used ✨

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hide-wp-admin-bar
 Want to hide WP Admin with simple JS? Here is the code, just copy and paste, simple! ✨
+
+## How to use this code?
+### Regular user
+1. Login to WordPress Dashboard
+2. Install Simple Custom CSS & JS plugin
+3. Create new custom JavaScript file
+4. Add the `code`
+5. Enjoy! ✨
+### Advanced user
+1. Login to WordPress Dashboard
+2. Add the `code` to header.php in your theme file
+3. Violla! ✨
+
+Easy isn't it? If you find useful, don't forget to click te stars! ✨
+
+Thank you!
---
+++ hide-wp-admin-bar.js
@@ -0,0 +1,29 @@
+// Get the WP Admin Bar element
+var adminBar = document.querySelector('#wpadminbar');
+// Timeout to set if you hovering the WP Admin Bar
+var showDelay;
+
+// Check if page have WP Admin Bar
+if (adminBar) {
+    // Create style inside header tag
+    var style = document.createElement('style');
+    style.textContent = 'html { transition: margin 0.25s ease; margin-top: 0 !important; } #wpadminbar { transition: transform 0.25s ease, opacity 0.25s ease; } #wpadminbar.sp-hide { transform: translateY(-80%); opacity: 0; } #wpadminbar.sp-show { transform: translateY(0); opacity: 1; }';
+    document.head.appendChild(style);
+    // Initiate WP Admin Bar hide for the first time page show up
+    adminBar.classList.add('sp-hide');
+}
+
+// Show the WP Admin Bar if mouse over
+adminBar.addEventListener('mouseover', function () {
+    clearTimeout(showDelay);
+    adminBar.classList.add('sp-show');
+    adminBar.classList.remove('sp-hide');
+});
+
+// Hide the WP Admin Bar if mouse out after 1.5 seconds wait
+adminBar.addEventListener('mouseout', function () {
+    showDelay = setTimeout(function () {
+        adminBar.classList.add('sp-hide');
+        adminBar.classList.remove('sp-show');
+    }, 1500);
+});
